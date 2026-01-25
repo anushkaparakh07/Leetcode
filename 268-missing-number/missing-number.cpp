f1990fa8e7212b0e4d54a99c273bb03a000f33ee
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-01-25 09:24:56 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (6.81%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> arr(n + 1, 0);
+        for(int i=0;i<n;i++){
+            arr[nums[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
