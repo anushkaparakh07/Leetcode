23f348f8ef507cd1113abcc188009056a17d9207
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-05-12 21:43:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (91.87%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        vector<int>arr(26,0);
+        int arr[26]={0};
+        int size = sizeof(arr)/sizeof(arr[0]);
         for(char ch:s){
             arr[ch-'a']++;
         }
         for(char ch1:t){
             arr[ch1-'a']--;
         }
-        for(int i=0;i<arr.size();i++){
+        for(int i=0;i<size;i++){
             if(arr[i]!=0) return false;
         }
         return true;
