00cd8c6051cff4504b48e4f859094bd322a1bf39
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-05-16 14:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.59%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+int f(int n){
+    vector<int> m(n + 1, -1);
+    m[0]=1;
+    m[1]=1;
+    for(int i=2;i<=n;i++){
+        m[i] = m[i-1]+m[i-2];
+    }
+    return m[n];
+}
+class Solution {
+public:
+    int climbStairs(int n) {
+        return f(n);
+    }
+};
