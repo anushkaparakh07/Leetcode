315e25c32992d5550dad921bc621c77e10fd3eae
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-06-08 08:33:41 +0530

    Time: 11 ms (77.61%) | Memory: 15.7 MB (76.35%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+               ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U';
+    }
+    string sortVowels(string s) {
+       string vowel;
+       for(char ch:s){
+        if(isVowel(ch)){
+            vowel.push_back(ch);
+        }
+       } 
+       sort(vowel.begin(),vowel.end());
+       int index = 0;
+       for(int i=0;i<s.size();i++){
+        if(isVowel(s[i])){
+            s[i] = vowel[index];
+            index++;
+        }
+       }
+       return s;
+    }
+};
