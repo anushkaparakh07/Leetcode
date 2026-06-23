d41a75d9367d0e00249dbf901eb504f8c30ae06f
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-06-23 09:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (39.13%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,12 +5,21 @@ public:
         if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
-        int l = 1, h = n-2;      
+        int l = 1 , h = n-2;
         while(l<=h){
-            int mid = (l+h)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid]>nums[mid-1]) l = mid+1;
-            else h = mid-1;
+            int mid = l+(h-l)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1] ){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                l = mid+1;
+            }
+            // else if(nums[mid]<nums[mid-1]){
+            //     h = mid-1;
+            // }
+            else{
+                h = mid-1;
+            }
         }
         return -1;
     }
