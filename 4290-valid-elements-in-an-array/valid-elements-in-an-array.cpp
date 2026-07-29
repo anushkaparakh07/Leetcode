9233d4d50122d6171b28cfff1c10fad383baa4ea
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-07-29 15:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (38.87%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findValidElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>rightMax(n);
+        rightMax[n-1] = INT_MIN;
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = max(rightMax[i+1],nums[i+1]);
+        }
+        vector<int>ans;
+        int leftMax = nums[0];
+        for(int i=0;i<n;i++){
+            if(i==0||i==n-1||nums[i]>leftMax||nums[i]>rightMax[i]){
+                ans.push_back(nums[i]);
+            }
+            leftMax = max(leftMax,nums[i]);
+        }
+        return ans;
+    }
+};
