4b0906512db5017461d464cd5c9b6fb264625d23
Anushka Parakh <138969777+anushkaparakh07@users.noreply.github.com>
2026-08-09 16:30:53 +0530

    Time: 3 ms (16.21%) | Memory: 9.2 MB (86.69%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        map<char,int>mpp;
-        for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
-        }
-        int longSum = 0;
-        for(auto it:mpp){
-            if(it.second%2==0) longSum+=it.second;
-            else longSum+=(it.second-1);
-        }
-        for(auto it:mpp){
-            if(it.second%2!=0) return longSum+=1;
-        }
-        return longSum;
+     map<char,int>mpp;
+     int sum =0;
+      bool hasOdd = false;
+     for(int i=0;i<s.size();i++){
+        mpp[s[i]]++;
+     }   
+     for(auto it:mpp){
+        if(it.second%2==0) sum+=it.second;
+        else{
+            sum+=(it.second-1);
+            hasOdd = true;
+        };
+     }
+     if(hasOdd) return sum + 1;
+     return sum;
     }
 };
